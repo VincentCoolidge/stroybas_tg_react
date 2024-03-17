b6f97dc326587733b7e67a50b4d0eb49fe822a8a
Anatoly <anatolysavchenko95@gmail.com>
2024-03-17 18:18:03 +0300

    update

--- src/pages/MockData.js
+++ src/pages/MockData.js
@@ -1,4 +1,12 @@
 import ProjectImg1 from "../assets/project/attic/1_project/1.jpg";
+import ProjectImg2 from "../assets/project/attic/2_project/2.jpg";
+import ProjectImg3 from "../assets/project/attic/3_project/3.jpg";
+import ProjectImg4 from "../assets/project/one-story/1_project/1.jpeg";
+import ProjectImg5 from "../assets/project/one-story/2_project/2.jpg";
+import ProjectImg6 from "../assets/project/one-story/3_project/3.jpg";
+import ProjectImg7 from "../assets/project/two-story/1_project/1.jpg";
+import ProjectImg8 from "../assets/project/two-story/2_project/2.jpg";
+import ProjectImg9 from "../assets/project/two-story/3_project/3.jpg";
 
 export const projects = [
   {
@@ -11,41 +19,49 @@ export const projects = [
     id: "project_2",
     title:
       "1 этаж, площадь застройки 124 кв.м., площадь здания 82,8кв.м., 3 спальни, 2 с/у, терраса",
+    img: <ProjectImg2 />,
   },
   {
     id: "project_3",
     title:
       "1 этаж, площадь застройки 156,8 кв.м., площадь здания 110кв.м., 4 спальни, 2 с/у, терраса",
+    img: <ProjectImg3 />,
   },
   {
     id: "project_4",
     title:
       "мансардный, площадь застройки 77 кв.м., площадь здания 49кв.м., 2 спальни, подходит для участков с фасадом от 13метров!",
+    img: <ProjectImg4 />,
   },
   {
     id: "project_5",
     title:
       "мансардный, площадь застройки 107,2 кв.м., площадь здания 70кв.м., 2 спальни, гостевая комната, терраса",
+    img: <ProjectImg5 />,
   },
   {
     id: "project_6",
     title:
       "мансардный, площадь застройки 107,2 кв.м., площадь здания 70кв.м., 2 спальни, гостевая комната, терраса",
+    img: <ProjectImg6 />,
   },
   {
     id: "project_7",
     title:
       "2 этажа, площадь застройки 127 кв.м., площадь здания 163,6кв.м., 4 спальни, кабинет, 2 с/у, терраса",
+    img: <ProjectImg7 />,
   },
   {
     id: "project_8",
     title:
       "2 этажа, площадь застройки 98,7 кв.м., площадь здания 129кв.м., 3 спальни, кабинет, 2 с/у, терраса",
+    img: <ProjectImg8 />,
   },
   {
     id: "project_9",
     title:
       "2 этажа, площадь застройки 127 кв.м., площадь здания 180кв.м., 4 спальни, 3 с/у, и террасы",
+    img: <ProjectImg9 />,
   },
 ];
 
